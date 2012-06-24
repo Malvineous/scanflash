8c16a09c7d82d23e1c3a3e53156e796b5a4a2560
Adam Nielsen <a.nielsen@shikadi.net>
2012-06-24 16:49:06 +1000

    Autoconf

---
+++ autogen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Remove M4 files automatically copied (in case autotools version changes)
+rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
+
+autoreconf -fi || exit $?
+
+rm -Rf autom4te*.cache
